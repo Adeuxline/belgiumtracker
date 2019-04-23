39a19390da4810110af737c675ac0e9b3915e014
root <root@LAPTOP-21TDJLH6.localdomain>
2019-04-23 13:17:47 +0200

    Commentaires afin d'améliorer la compréhension

--- extraction/commissions/commission.py
+++ extraction/commissions/commission.py
@@ -2,12 +2,8 @@ from bs4 import BeautifulSoup
 from bs4 import SoupStrainer
 import urllib
 import re
-import json
 
 from functions import *
-
-def create_dic(name):
-    return
 
 def full(doc):
     commission = {}
@@ -30,59 +26,80 @@ def full(doc):
             break
         else:
             intro.append(doc[line].strip())
-    commission['introduction'] = intro
+    commission["introduction"] = intro
 
-    # ordre du jour
-    commission['odj'] = {}
-    commission['reunion'] = {}
+    # ordre du jour : uniquement les points principaux. Pour voir les détails, voir "reunion"
+    commission["odj"] = {}
+    commission["reunion"] = {}
     for line in range(line, len(doc)):
+        # recherche des lignes commençant ayant en début de phrase deux ciffres, un espace, une majuscule et une suite de minuscules
+        # EXEMPLE : "01 Charles"
         regex = re.compile('^[0-9]{2}\s[A-Z][a-z]+')
         if regex.match(doc[line]):
+            # coupe en deux le point de l'odre du jour grâce au numéro au milieu de la ligne qui permet de voir quand commence la traduction du point
+            # EXEMPLE : " 01 " ou " 123 "
             point = re.compile('\s[0-9]+\s').split(doc[line])
+            # récupère le numero du point de l'odj
             pointOdj = point[0][:2]
-            commission['odj'][pointOdj] = {}
-            commission['odj'][pointOdj]['fr'] = point[0][3:].strip()
-            commission['odj'][pointOdj]['nl'] = point[1].strip()
+            commission["odj"][pointOdj] = {}
+            # récupère la première phrase du point (fr/nl... On ne sait pas) en retirant son numéro
+            commission["odj"][pointOdj]["fr"] = point[0][3:].strip()
+            # récupère la deuxième phrase du point
+            commission["odj"][pointOdj]["nl"] = point[1].strip()
             
-            commission['reunion'][pointOdj] = {}
+            commission["reunion"][pointOdj] = {}
+
+            # recherche des sous-points dans le PV
             new_line = line + 1
+            # recherche des lignes ayant en début de phrase deux chiffres, un point, deux chiffres, un espace
+            # EXEMPLE : "01.04 "
             new_regex = re.compile('^[0-9]{2}[.][0-9]{2}\s')
             for new_line in range(new_line, len(doc)):
                 if new_regex.match(doc[new_line]):
                     
+                    # dans "01.04", le souspoint correspond à "04"
                     souspoint = doc[new_line][3] + doc[new_line][4]
-                    commission['reunion'][pointOdj][souspoint] = {}
+                    commission["reunion"][pointOdj][souspoint] = {}
 
-                    commission['reunion'][pointOdj][souspoint]['intervention'] = doc[new_line][6:].strip()
-                    commission['reunion'][pointOdj][souspoint]['intervenant'] = {}
+                    # la première ligne du souspoint est l'intervention
+                    commission["reunion"][pointOdj][souspoint]["intervention"] = doc[new_line][6:].strip()
+                    commission["reunion"][pointOdj][souspoint]["intervenant"] = {}
                     
-                    intervenant = commission['reunion'][pointOdj][souspoint]['intervention'].split(':')[0].strip()
+                    # Une intervention commence toujours par "Nom Prenom (parti):" ou par "Nom Prenom, minisre:". On récupère donc ce qu'il y a avant les ":"
+                    intervenant = commission["reunion"][pointOdj][souspoint]["intervention"].split(':')[0].strip()
                     
+                    # Version NL, le "Minister" vient devant le nom et prénom
                     if 'Minister' in intervenant:
-                        commission['reunion'][pointOdj][souspoint]['intervenant']['nom'] = intervenant.split(' ', 1)[1].strip()
-                        commission['reunion'][pointOdj][souspoint]['intervenant']['statut'] = 'Ministre'
+                        # on coupe en deux sur le premier espace et on récupère la deuxième partie
+                        commission["reunion"][pointOdj][souspoint]["intervenant"]["nom"] = intervenant.split(' ', 1)[1].strip()
+                        commission["reunion"][pointOdj][souspoint]["intervenant"]["statut"] = 'Ministre'
                     elif '(' in intervenant:
-                        commission['reunion'][pointOdj][souspoint]['intervenant']['nom'] = intervenant.split('(')[0].strip()
-                        commission['reunion'][pointOdj][souspoint]['intervenant']['statut'] = intervenant.split('(')[1].strip().replace(')', '')
+                        # on coupe en deux sur "(", et on récupère la première partie pour le nom, la deuxième partie pour le parti
+                        commission["reunion"][pointOdj][souspoint]["intervenant"]["nom"] = intervenant.split('(')[0].strip()
+                        commission["reunion"][pointOdj][souspoint]["intervenant"]["statut"] = intervenant.split('(')[1].strip().replace(')', '')
                     elif ',' in intervenant:
-                        commission['reunion'][pointOdj][souspoint]['intervenant']['nom'] = intervenant.split(',')[0].strip()
-                        commission['reunion'][pointOdj][souspoint]['intervenant']['statut'] = 'Ministre'
+                        # on coupe en deux sur "," et on récupère la première partie pour le nom. Le statut est "Ministre" d'office. Version FR de la première condition
+                        commission["reunion"][pointOdj][souspoint]["intervenant"]["nom"] = intervenant.split(',')[0].strip()
+                        commission["reunion"][pointOdj][souspoint]["intervenant"]["statut"] = 'Ministre'
 
+                    # Récupération de la suite des lignes concernant l'intervention
                     details = []
                     second_line = new_line + 1
-                    commission['reunion'][pointOdj][souspoint]['details'] = []
+                    commission["reunion"][pointOdj][souspoint]["details"] = []
                     for second_line in range(second_line, len(doc)):
+                        # si la ligne corresponf à un souspoint ou à un point de l'odj, on s'arrête. Sinon, on récupère la ligne.
                         if new_regex.match(doc[second_line]) or regex.match(doc[second_line]):
                             break
                         else:
-                            commission['reunion'][pointOdj][souspoint]['details'].append(doc[second_line].strip())
+                            commission["reunion"][pointOdj][souspoint]["details"].append(doc[second_line].strip())
                     
-                
+                # si la ligne correspond à un point de l'odre du jour, on s'arrête
                 if regex.match(doc[new_line]):
                     break
     
     return commission
 
+# récupération du document en ligne et préparation de ce dernier pour utilisation
 def init(commission):
     url = 'http://www.lachambre.be/doc/CCRI/html/54/ic' + str(commission) + 'x.html'
     html_doc = urllib.request.urlopen(url)
