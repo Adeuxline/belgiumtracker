14c43737451ee0be3e582c3743aaaafc24acae57
Richard Mathot <richard.mathot@gmail.com>
2019-08-04 11:46:13 +0200

    Allow debug mode on frontend pages

--- belgium_tracker/controllers/main.py
+++ belgium_tracker/controllers/main.py
@@ -6,26 +6,26 @@ from odoo.http import request
 class VotesController(http.Controller):
 
     @http.route(['/deputes'], type='http', auth='public', website=True)
-    def deputes(self):
+    def deputes(self, **kwargs):
         deputes = request.env['belgium_tracker.depute'].search([])
         # TODO search last legislature only
         # last_legislature = request.env['belgium_tracker.legislature'].search([], limit=1)
         return request.render('belgium_tracker.deputes', {'deputes': deputes})
 
     @http.route(['/depute/<model("belgium_tracker.depute"):depute>'], type='http', auth='public', website=True)
-    def depute(self, depute):
+    def depute(self, depute, **kwargs):
         choix = request.env['belgium_tracker.choix'].search([('depute_id', '=', depute.id)])
         return request.render('belgium_tracker.depute', {'depute': depute, 'choix': choix})
 
     @http.route(['/seances'], type='http', auth='public', website=True)
-    def seances(self):
+    def seances(self, **kwargs):
         seances = request.env['belgium_tracker.seance'].search([])
         return request.render('belgium_tracker.seances', {'seances': seances})
 
     @http.route(['/seance/<model("belgium_tracker.seance"):seance>'], type='http', auth='public', website=True)
-    def seance(self, seance):
+    def seance(self, seance, **kwargs):
         return request.render('belgium_tracker.seance', {'seance': seance})
 
     @http.route(['/vote/<model("belgium_tracker.vote"):vote>'], type='http', auth='public', website=True)
-    def vote(self, vote):
+    def vote(self, vote, **kwargs):
         return request.render('belgium_tracker.vote', {'vote': vote})
